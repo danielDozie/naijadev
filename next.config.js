13986a397dfc6be05c1132f00be67c03e0652fa7
niel63 <placmedia@gmail.com>
2021-07-19 12:29:45 +0100

    updated git ignore

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     env: {
-      API_URL : 'https://nairaavenue.herokuapp.com/devs',
-      // API_URL : 'https://localhost:1337/devs',
+      // API_URL : 'https://nairaavenue.herokuapp.com/devs',
+      API_URL : 'https://localhost:1337/devs',
       IMAGE_URL : 'https://nairaavenue.herokuapp.com',
       SiteTitle : 'Dev.Hub',
       APOLLO_URL : 'https://nairaavenue.herokuapp.com/graphql',
-      BLOG_API : 'https://nairaavenue.herokuapp.com/blogs', 
-      // BLOG_API : 'http://localhost:1337/blogs',
+      // BLOG_API : 'https://nairaavenue.herokuapp.com/blogs', 
+      BLOG_API : 'http://localhost:1337/blogs',
       images: {
         domains: ['res.cloudinary.com'],
       }, 
